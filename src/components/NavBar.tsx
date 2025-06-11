3d33a2c03590edff832527490314e0416f620f5e
hoijoii <oodh3328@gmail.com>
2025-06-11 11:09:16 +0900

    Add: navbar 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,45 @@
+import menuItems from '@/assets/data/menu-info.json';
+import CartIcon from '@/components/icons/CartIcon';
+import avatar from '@/assets/images/avatar/image-avatar.png';
+import { useNavigate } from 'react-router-dom';
+
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const handleGoToPage = (page: string) => {
+    navigate(page);
+  };
+
   return (
-    <div>
-      
+    <div id="navbar">
+      <div className="nav-container">
+        <div className="nav-content">
+          <div className="nav-lft">
+            <div className="logo" onClick={() => handleGoToPage('/')}>
+              sneakers
+            </div>
+            <div className="menu">
+              <ul className="list">
+                {menuItems.map(item => {
+                  return (
+                    <li key={item.id} onClick={() => handleGoToPage(item.url)}>
+                      {item.name}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="nav-rgt">
+            <div className="cart">
+              <CartIcon />
+            </div>
+            <div className="avatar">
+              <img src={avatar} alt="avatar" width="40" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/icons/CartIcon.tsx
@@ -0,0 +1,7 @@
+import CartSvg from '@/assets/images/icons/icon-cart.svg?react';
+
+const CartIcon = () => {
+  return <CartSvg />;
+};
+
+export default CartIcon;
