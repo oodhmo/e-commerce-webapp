05475c6b413162a87108a056512219ddd9cf79e7
hoijoii <oodh3328@gmail.com>
2025-06-27 17:57:16 +0900

    Add: 디테일페이지 팝업 컴포넌트 추가

---
+++ src/components/detail/GalleryPopup.tsx
@@ -0,0 +1,14 @@
+const GalleryPopup = () => {
+  return (
+    <div id="dialog">
+      <div className="dialog-container">
+        <div className="x-btn">{/* close btn */}</div>
+        <div className="pictures">
+          <div className="slider"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GalleryPopup;
