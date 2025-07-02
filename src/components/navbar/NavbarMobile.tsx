9824adf56143e934b34f0df231bf44ce4ee6e18e
hoijoii <oodh3328@gmail.com>
2025-07-02 18:25:36 +0900

    Add: navbar 분기 추가

--- src/components/navbar/NavbarMobile.tsx
+++ src/components/navbar/NavbarMobile.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import menuItems from '@/assets/data/menu-info.json';
+import CartIcon from '@/components/icons/CartIcon';
+import avatar from '@/assets/images/avatar/image-avatar.png';
 
 const NavbarMobile = () => {
-  return <div></div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleGoToPage = (page: string) => {
+    navigate(page);
+    setMenuOpen(false); // 메뉴 닫기
+  };
+
+  return (
+    <div id="navbar-mobile">
+      <div className="nav-container"></div>
+    </div>
+  );
 };
 
 export default NavbarMobile;
