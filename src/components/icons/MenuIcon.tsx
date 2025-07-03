c953bd48b084dd565cce9a85a7cc15f47bd6b7e5
hoijoii <oodh3328@gmail.com>
2025-07-03 13:39:03 +0900

    Feat: 모바일 navbar 추가

---
+++ src/components/icons/MenuIcon.tsx
@@ -0,0 +1,7 @@
+import MenuSvg from '@/assets/images/icons/icon-menu.svg?react';
+
+const MenuIcon = () => {
+  return <MenuSvg />;
+};
+
+export default MenuIcon;
--- src/components/navbar/NavbarMobile.tsx
+++ src/components/navbar/NavbarMobile.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import menuItems from '@/assets/data/menu-info.json';
 import CartIcon from '@/components/icons/CartIcon';
+import MenuIcon from '@/components/icons/MenuIcon';
 import avatar from '@/assets/images/avatar/image-avatar.png';
 
 const NavbarMobile = () => {
@@ -13,9 +14,35 @@ const NavbarMobile = () => {
     setMenuOpen(false); // 메뉴 닫기
   };
 
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div id="navbar-mobile">
-      <div className="nav-container"></div>
+      <div className="nav-container">
+        <div className="nav-content">
+          <div className="nav-lft">
+            <div className="menu-btn" onClick={handleMenuToggle}>
+              <MenuIcon />
+            </div>
+            <div
+              className="logo"
+              onClick={() => handleGoToPage('/collections')}
+            >
+              sneakers
+            </div>
+          </div>
+          <div className="nav-rgt">
+            <div className="cart">
+              <CartIcon />
+            </div>
+            <div className="avatar">
+              <img src={avatar} alt="avatar" width="40" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
